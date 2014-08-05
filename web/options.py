f5876b2ed743b8e0d145239f85ec3a5bb9a8da3c
CubicDolphin <Alex@Alex-Wolfs-MacBook-4.local>
2014-08-05 12:00:30 -0700

    Clarified “options.py” a bit. Added an “alt” for the logo.

--- web/options.py
+++ web/options.py
@@ -2,10 +2,10 @@ time_zone = 'UTC'				   	#the three letter code, for example "UTC" for Pacific.
 
 company_name = 'Marketlive'		   	#your company's name.
 
-from_email = 'wolfa97@comcast.net' 	#address you want all emails from this website to come from. Full address!
-email_host = 'smtp.comcast.net'    	#smtp.something.com
-email_username = 'wolfa97'		   	#what you would use to log onto your email
-email_password = 'Willy2006'	   	#what you would use to log onto your email
+from_email = 'wolfa97@comcast.net' 	#email address you want all emails from this website to come from. Full address!
+email_host = 'smtp.comcast.net'    	#smtp.something.com/net, or smtp.everything_after_the_@_in_your_email (most likely)
+email_username = 'wolfa97'		   	#what you would use to log onto this email account
+email_password = 'Willy2006'	   	#what you would use to log onto this email account
 email_port = 587				   	#optimal port varies per host. You may need to look it up online. (Probably 587)
 
 #Put your company's logo into "Base/static/Base".
